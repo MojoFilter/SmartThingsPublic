774783386c884698fd8e20b04b78b8249fe9f0e7
Josh Weeks <weeks.josh@gmail.com>
2017-08-27 11:20:10 -0400

    Add Back To Bed

---
+++ smartapps/mojofilter/back-to-bed.src/back-to-bed.groovy
@@ -0,0 +1,46 @@
+definition(
+    name: "Back To Bed",
+    namespace: "MojoFilter",
+    author: "Josh Weeks",
+    description: "Turn specified lights back off after a delay when a switch is active.",
+    category: "",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+preferences {
+	section("Turn off these lights..."){
+		input "lights", "capability.switch", multiple: true
+	}
+	section("After a delay of..."){
+		input "delayMinutes", "number", title: "Minutes?"
+	}
+	section("If this switch is set") {
+		input "masterSwitch", "capability.switch", required: true
+	}
+}
+
+def installed() {
+	initialize()
+}
+
+def updated() {
+	unsubscribe()
+	unschedule()
+	initialize()
+}
+
+def initialize() {
+    subscribe(lights, "switch.on", lightOnHandler)	
+	astroCheck()
+}
+
+def lightOnHandler(evt) {
+    if (evt.value == "on" && masterSwitch.currentSwitch == "on") {
+        runIn(delayMinutes * 60, turnEmOff)
+    }
+}
+
+def turnEmOff() {
+    lights.off()
+}
